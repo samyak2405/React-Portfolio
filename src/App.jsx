e433ce377b6260759c8d8988efa040b0518575ce
Samyak moon <moonsamyak54@gmail.com>
2024-09-07 13:41:09 +0530

    Routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,28 @@ import Services from "./Components/Services/Services";
 import MyWork from "./Components/MyWork/MyWork";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <BroswerRouter>
+        <Routes>
+          <Route path="/React-Portfolio/navbar" element={<Navbar />} />
+          <Route path="/React-Portfolio/home" element={<Hero />} />
+          <Route path="/React-Portfolio/about" element={<About />} />
+          <Route path="/React-Portfolio/services" element={<Services />} />
+          <Route path="/React-Portfolio/mywork" element={<MyWork />} />
+          <Route path="/React-Portfolio/contact" element={<Contact />} />
+          <Route path="/React-Portfolio/footer" element={<Footer />} />
+        </Routes>
+      </BroswerRouter>
+      {/* <Navbar />
       <Hero />
       <About />
       <Services />
       <MyWork />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
